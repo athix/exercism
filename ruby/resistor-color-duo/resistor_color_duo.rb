eb8cbaf1a7018dc3bb45cdc8c6c72d5bbe077a21
Joshua Buker <buker.joshua@gmail.com>
2019-11-21 19:06:15 -0800

    Solve resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,36 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
+class ResistorColorDuo
+  def self.value(colors)
+    raise ArgumentError unless colors.is_a?(Array)
+    raise ArgumentError unless colors.all?{ |c| c.is_a?(String) }
+    total_resistance =
+      resistance(colors[0]).to_s + resistance(colors[1]).to_s
+    total_resistance.to_i
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
-
+  def self.resistance(color)
+    case color.downcase
+    when 'black'
+      0
+    when 'brown'
+      1
+    when 'red'
+      2
+    when 'orange'
+      3
+    when 'yellow'
+      4
+    when 'green'
+      5
+    when 'blue'
+      6
+    when 'violet'
+      7
+    when 'grey'
+      8
+    when 'white'
+      9
+    else
+      raise ArgumentError
+    end
+  end
+end
