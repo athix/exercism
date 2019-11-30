9056fd64dcf4ca703f9265edf2ce5ace12663bd1
Joshua Buker <buker.joshua@gmail.com>
2019-11-30 14:43:54 -0800

    Copy matrix solution to team

---
+++ teams/big-brain-energy/ruby/matrix/matrix.rb
@@ -0,0 +1,20 @@
+class Matrix
+  attr_accessor :matrix
+
+  def initialize(matrix)
+    rows = matrix.split("\n")
+    self.matrix = []
+
+    rows.each do |row|
+      self.matrix << row.split(' ').map(&:to_i)
+    end
+  end
+
+  def rows
+    matrix
+  end
+
+  def columns
+    matrix.transpose
+  end
+end
