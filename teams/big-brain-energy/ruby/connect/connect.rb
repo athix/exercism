329e614efa318024bc73c37838dcb1afcdf4f274
Joshua Buker <buker.joshua@gmail.com>
2019-11-30 16:04:24 -0800

    Connect exercise WIP

--- teams/big-brain-energy/ruby/connect/connect.rb
+++ teams/big-brain-energy/ruby/connect/connect.rb
@@ -1,11 +1,32 @@
 class Board
   attr_accessor :board
 
+  # Player O = Top/Bottom
+  # Player X = Left/Right
+
   def initialize(board)
     self.board = board
+    self.visualize
+  end
+
+  def visualize
+    output = ""
+    board.each_with_index do |row, index|
+      index.times { output += ' ' }
+      output += row + "\n"
+    end
+    puts "\n"
+    puts output
+    puts "\n"
+    self
   end
 
   def winner
     ''
+  end
+
+  # Credit to: https://www.redblobgames.com/grids/hexagons/
+  # https://www.redblobgames.com/grids/hexagons/implementation.html
+  class HexGrid
   end
 end
